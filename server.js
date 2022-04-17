8a1a9dedfc8f92af073c21d844e4e8457d766ff3
canon <eirinlove@gmail.com>
2022-04-17 16:39:06 +0900

    React+Express.js연동

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+
+const http = require('http').createServer(app);
+http.listen(8086, function(){
+
+    console.log('listen 8080');
+});
+
+
+app.use( '/', express.static(path.join(__dirname, 'public')))
+
+app.use ( '/re', express.static( path.join(__dirname, 'react-project/build'))) // 리액트 전환
+
+
+app.get('/', function(req,res){
+
+res.sendFile( path.join(__dirname, 'public/index.html'))
+
+})
+
+app.get('/re', function(req,res){
+
+res.sendFile( path.join(__dirname, 'react-project/build.index.html'))
+
+})
